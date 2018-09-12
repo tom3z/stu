bbc5048dd55d595056db9220df20dc7e2f257c36
tom3z <1443626199@qq.com>
2018-09-12 14:30:25 +0800

    jttest-2

---
+++ jt-gateway-zuul/src/main/java/cn/jt/RunAppZuul.java
@@ -0,0 +1,15 @@
+package cn.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy	//api网关
+@EnableEurekaClient
+public class RunAppZuul {
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppZuul.class, args);
+	}
+}
